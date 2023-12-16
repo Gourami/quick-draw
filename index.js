59b7f6e0345a26c0bac46be87fa55b845d1c9382
Tyler Dill <tylerjdill@gmail.com>
2023-12-15 20:48:22 -0500

    Import and export all project classes

---
+++ index.js
@@ -0,0 +1,6 @@
+import BufferObject from "./Engine/BufferObject";
+import GLUtilities from "./Engine/GLUtilities";
+import Matrix4 from "./Engine/Matrix4";
+import Vector3 from "./Engine/Vector3";
+
+export {BufferObject, GLUtilities, Matrix4, Vector3};
