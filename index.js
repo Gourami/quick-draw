b34867c8f1430a251e5a4906edf730708396c5e0
Tyler Dill <tylerjdill@gmail.com>
2023-12-25 17:22:45 -0500

    Add file extension to fix ES module import

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import BufferObject from "./Engine/BufferObject";
-import GLUtilities from "./Engine/GLUtilities";
-import Matrix4 from "./Engine/Matrix4";
-import Vector3 from "./Engine/Vector3";
+import BufferObject from "./Engine/BufferObject.js";
+import GLUtilities from "./Engine/GLUtilities.js";
+import Matrix4 from "./Engine/Matrix4.js";
+import Vector3 from "./Engine/Vector3.js";
 
 // export public properties for use in module
-export {BufferObject, GLUtilities, Matrix4, Vector3};
+export { BufferObject, GLUtilities, Matrix4, Vector3 };
